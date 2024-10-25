d7d3961f8c069325a6d21fb429c495971ca1a0f3
Steven <300141601@monboreal.ca>
2024-10-25 16:07:38 -0400

    ajout

---
+++ AppPaiements/Utilisateur.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Utilisateur
+    {
+        public string Nom { get; set; }
+        public List<Paiement> paiements;
+
+        public Utilisateur(string nom)
+        {
+            Nom = nom;
+            paiements = new List<Paiement>();
+        }
+
+        public void AfficherInfos()
+        {
+            Console.WriteLine($"L'utilisateur est : {Nom}");
+            foreach (var paiement in paiements)
+            {
+                paiement.AfficherDetails();
+            }
+        }
+
+        public void AjouterPaiement(Paiement paiement)
+        {
+            paiements.Add(paiement);
+        }
+    }
+}
